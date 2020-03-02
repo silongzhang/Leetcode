48a40b4893b92ffa7fa71aa79e6fe701453889e5
along <along1994@sjtu.edu.cn>
2020-03-02 16:17:58 +0800

    2020.03.02.16.17

---
+++ Header.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <ilcplex/ilocplex.h>
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<thread>
+#include<mutex>
+#include<ctime>
+#include<random>
+#include<string>
+#include<vector>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<queue>
+#include<tuple>
+#include<bitset>
+#include<algorithm>
+
+
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::ifstream;
+using std::ofstream;
+using std::ostringstream;
+using std::exception;
+using std::thread;
+using std::mutex;
+using std::lock_guard;
+using std::default_random_engine;
+using std::uniform_real_distribution;
+using std::normal_distribution;
+using std::string;
+using std::vector;
+using std::set;
+using std::multiset;
+using std::unordered_set;
+using std::map;
+using std::multimap;
+using std::queue;
+using std::tuple;
+using std::pair;
+using std::make_pair;
+using std::make_tuple;
+using std::get;
+using std::bitset;
+using std::ref;
+using std::min;
+using std::max;
+
