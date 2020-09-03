c17675a2a6fa814168f165634cdfad690f7f0799
along <along1994@sjtu.edu.cn>
2020-09-03 13:00:13 +0800

    2020.09.03.13.00

---
+++ 75.h
@@ -0,0 +1,22 @@
+// 75. —’…´∑÷¿‡
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	void sortColors(vector<int>& nums) {
+		int left = 0, current = 0, right = nums.size() - 1;
+		while (current <= right) {
+			if (nums[current] == 0)
+				swap(nums[left++], nums[current++]);
+			else if (nums[current] == 1)
+				++current;
+			else
+				swap(nums[current], nums[right--]);
+		}
+	}
+};
+
